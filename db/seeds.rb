bd988f281503cdb106137b1556667a26ccdc8376
Alexandre Duquenne <duquenne.alexandre22@gmail.com>
2016-05-31 14:49:09 +0200

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+# require "faker"
+10.times do
+  post = Article.create({
+                    title: Faker::Company.name,
+                    content: Faker::Lorem.paragraph(word_count=250)
+                  })
+  post.save
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +14,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
